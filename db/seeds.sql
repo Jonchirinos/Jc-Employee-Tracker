350eeee459de801c8220e89a6ee666e33902615a
Jon Chirinos <chirinosjon@gmail.com>
2022-03-12 15:25:32 -0600

    added seeding for tables

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+        ("Sales"),
+        ("Marketing");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Senior Developer", 150000, 1),
+        ("Junior Developer", 80000, 1),
+        ("Head of Sales", 115000, 2),
+        ("Sales Rep", 60000, 2),
+        ("Marketing VP", 150000, 3),
+        ("Marketing Associate", 70000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Jon", "Chirinos", 1, 1),
+        ("Kanye", "West", 2, NULL),
+        ("Wayne", "Brady", 3, 2),
+        ("Scott", "Brunswick", 4, 3);
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 const db = mysql.createConnection(
     {
-        host: localhost,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: "localhost",
+        user: "root",
+        password: "Illinois14",
+        database: "employee_db",
     },
     console.log("you are connected")
 );
@@ -24,10 +24,9 @@ function init() {
                 message: "What would you like to do?",
                 choices: [
                     { name: "view all employees", value: "view-emp" },
-                    {
-                        name: "view all departments",
-                        choices: "view-dep",
-                    },
+                    { name: "view all departments", Value: "view-dep" },
+                    { name: "view all roles", value: "view-role" },
+                    { name: "quit", value: "quit" },
                 ],
             },
         ])
@@ -45,4 +44,60 @@ function viewDepartments() {
     db.query(sql, (err, rows) => {
         console.table(rows);
     });
+}
+
+function viewRoles() {
+    db.query("SELECT * FROM roles", function (err, results) {
+        console.table(results);
+    });
+}
+
+function createEmployee() {
+    inquirer
+        .prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "What's the employee's first name?",
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "What's the employee's last name?",
+            },
+        ])
+        .then((answers) => {
+            db.query("SELECT * FROM roles", function (err, results) {
+                const roles = results.map(({ id, title }) => ({
+                    name: title,
+                    value: id,
+                }));
+                inquirer
+                    .prompt({
+                        type: "list",
+                        name: "id",
+                        message: "What is the employee's role?",
+                        choices: roles,
+                    })
+                    .then((role) => {
+                        db.query("SELECT * FROM employee where manager_id is null", function (err, results) {
+                            const managers = results.map(({ id, last_name }) => ({
+                                name: last_name,
+                                value: id,
+                            }));
+                            inquirer
+                                .prompt({
+                                    type: "list",
+                                    name: "id",
+                                    message: "What is the manager's name?",
+                                    choices: managers,
+                                })
+                                .then((manager) => {
+                                    db.query("INSERT INTO employee(first_name, last_name, role_id, manager_id) values(?,?,?,?)", [answers.firstName, answers.lastName, role.id, manager.id]);
+                                    init();
+                                });
+                        });
+                    });
+            });
+        });
 }
